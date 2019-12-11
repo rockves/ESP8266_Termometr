f62635dbba9b5d3ce9fc89febb49141d33a9a656
Miłosz <m.99@tlen.pl>
2019-12-11 22:13:53 +0100

    Displaying and sending temperature

--- src/main.cpp
+++ src/main.cpp
@@ -8,67 +8,62 @@
 #define PASSWORD "gn9j-0dln-2cp1"
 #define HOST "script.google.com"
 #define HTTP_PORT 443
-#define TEMP_SENSOR_GPIO 0
-#define TIME_BETWEEN_MEASUREMENT_MILISECONDS 500
-#define DEEP_SLEEP_TIME_MILISECONDS 5000
+#define TEMP_SENSOR_GPIO 0                                          //GPIO dla sensora DS18B20
+#define TIME_BETWEEN_CHECK_AND_READ_TEMPERATURE_MILISECONDS 800     //Czas między sprawdzeniem temperatury a jej odczytaniem z sensora
+#define TIME_BETWEEN_MEASUREMENT_MILISECONDS 200                    //Czas pomiędzy dwoma pomiarami
+#define TIME_BETWEEN_SENDING_DATA_MILISECONDS 10000                 //Czas po którym nastąpi wysłanie temperatury
+#define TIME_BETWEEN_DISPLAYING_DATA 2000                           //Czas pomiędzy wyświetleniem temperatury
 
-String url = "/macros/s/AKfycbz1tq8IRyaVZyfy-0BTZiumuc_s7IyiYQSKqXFcAe1IokDR__Me/exec?func=addData&";
+String url = "/macros/s/AKfycbz1tq8IRyaVZyfy-0BTZiumuc_s7IyiYQSKqXFcAe1IokDR__Me/exec?func=addData&"; //Link do makro w google script
 WiFiClientSecure client;
-OneWire oneWire(TEMP_SENSOR_GPIO);
-DallasTemperature sensor(&oneWire);
-U8X8_SSD1306_128X64_NONAME_SW_I2C oled(/* clock=*/ 5, /* data=*/ 4, /* reset=*/ U8X8_PIN_NONE);
-float probeTemperatures[3];
+
+OneWire oneWire(TEMP_SENSOR_GPIO); //Interfejs OneWire
+DallasTemperature sensor(&oneWire); //Obiekt sensora DS18B20
+
+U8X8_SSD1306_128X64_NONAME_SW_I2C oled(/* clock=*/ 5, /* data=*/ 4, /* reset=*/ U8X8_PIN_NONE); //Obiekt wyświetlacza OLED
+
+float probeTemperatures[2]; //Tablica pomiarów częściowych
+float avgTemperature = 0;   //Średnia temperatura liczona z pomiarów częściowych
+
+unsigned long timer_displaying = 0; //Kiedy odbyło się ostatnie wyświetlenie
+unsigned long timer_sending = 0;    //Kiedy odbyło się ostatnie wysłanie
+unsigned long time_read;            //Odczyt ile czasu program jest już uruchomiony
 
 void connectToWiFi(const char* _ssid, const char* _password){
-    digitalWrite(LED_BUILTIN, LOW);
-    if(DEBUG){ 
-        Serial.print("Try to connect to ");
-        Serial.print(_ssid);
-    }
     WiFi.mode(WIFI_STA);
     WiFi.begin(_ssid,_password);
-    while (WiFi.status() != WL_CONNECTED)
-    {
-        delay(500);
-        if(DEBUG) Serial.print(".");
-    }
-    if(DEBUG){
-        Serial.print("Connected\n");
-        Serial.println(WiFi.localIP());
-    }
-    digitalWrite(LED_BUILTIN, HIGH);
 }
 
 float getTemperature(){
-    probeTemperatures[0] = probeTemperatures[1] = probeTemperatures[2] = 0;
+    probeTemperatures[0] = probeTemperatures[1] = 0;
     
+    sensor.requestTemperatures();                                   //Prośba o sprawdzenie temperatury
+    delay(TIME_BETWEEN_CHECK_AND_READ_TEMPERATURE_MILISECONDS);     //Czekanie na odczyt   
+    probeTemperatures[0] = sensor.getTempCByIndex(0);               //Pobranie temperatury z czujnika
+
+    delay(TIME_BETWEEN_MEASUREMENT_MILISECONDS); //Czekanie między pomiarami
+
     sensor.requestTemperatures();
-    probeTemperatures[0] = sensor.getTempCByIndex(0);
-    delay(TIME_BETWEEN_MEASUREMENT_MILISECONDS);
-    sensor.requestTemperatures();
+    delay(TIME_BETWEEN_CHECK_AND_READ_TEMPERATURE_MILISECONDS);
     probeTemperatures[1] = sensor.getTempCByIndex(0);
-    delay(TIME_BETWEEN_MEASUREMENT_MILISECONDS);
-    sensor.requestTemperatures();
-    probeTemperatures[2] = sensor.getTempCByIndex(0);
+
     if(DEBUG){
-        Serial.print("Temperatura 1: ");
-        Serial.println(probeTemperatures[0]);
-        Serial.print("Temperatura 1: ");
-        Serial.println(probeTemperatures[1]);
-        Serial.print("Temperatura 1: ");
-        Serial.println(probeTemperatures[2]);
+        char temp[25];
+        float tempF;
+
+        tempF = probeTemperatures[0];
+        dtostrf(tempF, 2, 2, temp);
+        oled.drawString(0,4,strcat(temp, " - Temp 1"));
+
+        tempF = probeTemperatures[1];
+        dtostrf(tempF, 2, 2, temp);
+        oled.drawString(0,6,strcat(temp, " - Temp 2"));
     }
-    float avgTemp = (probeTemperatures[0]+probeTemperatures[1]+probeTemperatures[2])/3;
-    const char* degree = " °C";
-    oled.clear();
-    oled.clearDisplay();
-    oled.print(avgTemp);
-    oled.print(degree);
-    return (avgTemp);
-} //~1s
+    return ((probeTemperatures[0]+probeTemperatures[1])/2); //Zwrot uśrednionych odczytów
+} //~1800ms
 
 void sendData(float temperature){
-    if (!client.connect(HOST, HTTP_PORT)) {
+    if (!client.connect(HOST, HTTP_PORT)) { //Test czy klient połączył się do hosta przez HTTPS
         if(DEBUG){
             Serial.println("connection failed\n");
             Serial.print("My ip: ");
@@ -77,25 +72,49 @@ void sendData(float temperature){
         return;
     }
     if(DEBUG) Serial.print("Wysylam dane\n");
+    //Wysłanie GET na URL makra z temperaturą
     client.print(String("GET ") + url + "temp=" + String(temperature) + " HTTP/1.1\r\n" +
         "Host: " + HOST + "\r\n" +
         "User-Agent: ESP8266\r\n" +
         "Connection: close\r\n\r\n");
 }
 
+void displayTemperature(){
+    char temp[10]; //Tablica znaków dla wartości temperatury
+    dtostrf(avgTemperature, 2, 2, temp); //Konwersja temperatury średnioj na znaki
+    (avgTemperature>0) ? strcat(temp, "°C ") : strcat(temp, "°C"); //Dołączenie znaku stopni celcjusza
+    oled.drawString(0,2,temp); //Wypisanie na ekran
+}
+
 void setup(){
-    Serial.begin(115200);
-    pinMode(LED_BUILTIN, OUTPUT);
-    WiFi.persistent(false);
-    connectToWiFi(SSID,PASSWORD);
-    client.setInsecure();
-    sensor.begin();
-    oled.begin();
-    oled.setFont(u8x8_font_pcsenior_f);
+    //Serial.begin(115200);
+    oled.begin(); //Inicjalizacja ekranu
+    oled.setFont(u8x8_font_pcsenior_f); //Ustawienie czcionki
+    client.setInsecure(); //Ustawienie niezabezpieczonego połączenie klienta aby wysłać dane bez sprawdzania certyfikatu
+    sensor.begin(); //Inicjalizacja sensora
+    WiFi.persistent(true); //Ustawienie zapamiętania ustawień sieci WiFi
+
+    oled.drawString(0,0,"OFFLINE"); //Wypisanie na wyświetlaczu
+    displayTemperature(); //Wypisanie temperatury
+    connectToWiFi(SSID,PASSWORD); //Start łączenia z siecią
 }
 
 void loop(){ 
-    sendData(getTemperature());
-    delay(DEEP_SLEEP_TIME_MILISECONDS);
-    //ESP.deepSleep(DEEP_SLEEP_TIME_MILISECONDS);
+    time_read = millis(); //Odczyt czasu od początku programu
+
+    (WiFi.status() == WL_CONNECTED) ? oled.drawString(0,0,"ONLINE ") : oled.drawString(0,0,"OFFLINE"); //Sprawdzenie stanu połączenia WiFi
+    
+    if (time_read - timer_displaying >= TIME_BETWEEN_DISPLAYING_DATA || time_read - timer_sending >= TIME_BETWEEN_SENDING_DATA_MILISECONDS){ //Sprawdzenie czy wykonać pomiar temperatury
+        avgTemperature = getTemperature(); //Pomiar temperatury
+
+        if (time_read - timer_displaying >= TIME_BETWEEN_DISPLAYING_DATA){
+            displayTemperature(); //Wypisanie temperatury
+            timer_displaying = time_read; //Odświeżenie czasu ostatniego wypisania
+        }
+        
+        if (time_read - timer_sending >= TIME_BETWEEN_SENDING_DATA_MILISECONDS){
+            sendData(avgTemperature); //Wysłanie temperatury
+            timer_sending = time_read; //Odświeżenie czasu ostatniego wysłania
+        } 
+    }
 }
